b66b359d550edd5420f6474a9c6e996968e11353
Chris Beard <cbeard@hnoj.org>
2021-05-15 22:05:09 -0500

    changed local storage back to my original plan

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,8 +4,10 @@ let now = DateTime.local();
 let currentDay = document.getElementById("currentDay")
 currentDay.innerText = now.toLocaleString(DateTime.DATETIME_MED)
 
+//setting up rows for color changing
 
-let saveButton = document.querySelectorAll(".saveBtn");
+let saveButton = document.querySelector(".saveBtn");
+
 /*let comment = document.getElementById("description")*/
 
 function renderLastAppt() {
@@ -22,6 +24,7 @@ function renderLastAppt() {
   $('#hour6 .description').val(localStorage.getItem('hour6'));
 }
 
+
 saveButton.addEventListener("click", function (event) {
   event.preventDefault();
   let note = $(this).siblings('.description').val()
@@ -35,64 +38,3 @@ function init() {
 }
 init();
 
-
-
-
-/*
-
-const rows = document.getElementsByClassName("row time-block");
-let currentHour = parseInt(moment().format(''));
-
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === rowHour) {
-      setColor(row, "red");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
-    }
-  }
-});
-
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
-function hourTracker() {
-    //get current number of hours.
-    var currentHour = moment().hour();
-
-    // loop over time blocks
-    $("#row time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log( blockHour, currentHour)
-
-        //check if we've moved past this time
-        if (blockHour < currentHour) {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }
-        else if (blockHour === currentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
-    })
-}
-hourTracker();
-});*/
